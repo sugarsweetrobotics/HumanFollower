557b05bb013303d29c882674a1c43983b36ba9cf
ysuga <ysuga@ysuga.net>
2017-12-01 14:17:32 +0900

    first commit

---
+++ src/HumanFollower.cpp
@@ -0,0 +1,479 @@
+// -*- C++ -*-
+/*!
+ * @file  HumanFollower.cpp
+ * @brief Human Follow Commander for Mobile Robots
+ * @date $Date$
+ *
+ * @author Yuki Suga, Sugar Sweet Robotics
+ * @ysuga
+ *
+ * GPLv3
+ *
+ * $Id$
+ */
+
+#include "HumanFollower.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* humanfollower_spec[] =
+  {
+    "implementation_id", "HumanFollower",
+    "type_name",         "HumanFollower",
+    "description",       "Human Follow Commander for Mobile Robots",
+    "version",           "1.0.0",
+    "vendor",            "SUGAR SWEET ROBOTICS",
+    "category",          "MobileRobot",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.debug", "0",
+    "conf.default.vx_gain", "1.0",
+    "conf.default.va_gain", "1.0",
+    // Widget
+    "conf.__widget__.debug", "text",
+    "conf.__widget__.vx_gain", "text",
+    "conf.__widget__.va_gain", "text",
+    // Constraints
+    ""
+  };
+// </rtc-template>
+
+
+IplImage *pImages[2];
+uint32_t imageCount = 0;
+bool endFlag = false;
+
+const uint32_t WIDTH = 640;
+const uint32_t HEIGHT = WIDTH;
+
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+HumanFollower::HumanFollower(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_rangeIn("range", m_range),
+    m_velocityOut("velocity", m_velocity)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+HumanFollower::~HumanFollower()
+{
+}
+
+
+
+RTC::ReturnCode_t HumanFollower::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("range", m_rangeIn);
+  
+  // Set OutPort buffer
+  addOutPort("velocity", m_velocityOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("debug", m_debug, "0");
+  bindParameter("vx_gain", m_vx_gain, "1.0");
+  bindParameter("va_gain", m_va_gain, "1.0");
+  // </rtc-template>
+  
+
+  pImages[0] = cvCreateImage(cvSize(WIDTH, HEIGHT), IPL_DEPTH_8U, 3);
+  pImages[1] = cvCreateImage(cvSize(WIDTH, HEIGHT), IPL_DEPTH_8U, 3);
+
+  //pImages[1] = new cv::Mat(WIDTH, HEIGHT, CV_8UC3);
+
+
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t HumanFollower::onFinalize()
+{
+  endFlag = true;
+  return RTC::RTC_OK;
+}
+
+
+/*
+RTC::ReturnCode_t HumanFollower::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t HumanFollower::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+static int counter;
+
+const int MAX_OBJECTS = 12;
+CvScalar colors[MAX_OBJECTS] = {
+  CV_RGB(255,   0,   0),
+  CV_RGB(  0, 255,   0),
+  CV_RGB(  0,   0, 255),
+
+  CV_RGB(255, 255,   0),
+  CV_RGB(  0, 255, 255),
+  CV_RGB(255,   0, 255),
+
+  CV_RGB(255, 125,   0),
+  CV_RGB(  0, 255, 125),
+  CV_RGB(125,   0, 255),
+
+  CV_RGB(255, 125, 125),
+  CV_RGB(125, 255, 125),
+  CV_RGB(125, 125, 255),
+};
+
+RTC::ReturnCode_t HumanFollower::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t HumanFollower::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+struct Point {
+  double x;
+  double y;
+
+  Point(const double x, const double y): x(x), y(y) {}
+  Point(const Point& p): x(p.x), y(p.y) {}
+
+};
+
+bool operator==(const Point& p1, const Point& p2) {
+  return p1.x == p2.x && p1.y == p2.y;
+}
+
+struct Object {
+  std::vector<Point> points;
+
+  Object() {}
+
+  Object(const Point& p) {
+    points.push_back(p);
+  }
+
+  Object(const Object& o) {
+    points = o.points;
+  }
+};
+
+bool operator==(const Object& o1, const Object& o2) {
+  return o1.points == o2.points;
+}
+
+Point average(const Point& p0, const Point& p1) {
+  return Point((p0.x + p1.x)/2, (p0.y + p1.y)/2);
+}
+
+Point center(const Object& o) {
+  if (o.points.size() < 2) return Point(0,0);
+  return average(o.points[0], o.points[o.points.size()-1]);
+}
+
+double distance(const Point& p0, const Point& p1) {
+  double dx = p0.x - p1.x;
+  double dy = p0.y - p1.y;
+  return sqrt(dx*dx + dy*dy);
+}
+
+double width(const Object& o) {
+  if (o.points.size() == 0) return 0;
+  double dx = (o.points[0].x - o.points[o.points.size()-1].x);
+  double dy = (o.points[0].y - o.points[o.points.size()-1].y);
+  return sqrt(dx*dx + dy*dy);
+}
+
+Point range_to_point(const double range, const double angle) {
+  return Point(range * cos(angle), range * sin(angle));
+}
+
+const double meter_by_pixel = 0.008;
+int to_pixel(int max, const double x) { 
+  return max / 2 - x / meter_by_pixel;
+}
+
+CvPoint toCvPoint(const Point& p) {
+  return cvPoint(to_pixel(WIDTH, p.y), to_pixel(HEIGHT, p.x));
+}
+
+
+
+struct Human {
+  Point point;
+  double radius;
+  Object leg1;
+  Object leg2;
+  Human() : point(0,0), radius(0) {};
+  Human(const Object&leg1, const Object& leg2): leg1(leg1), leg2(leg2),
+						point(average(center(leg1), center(leg2))),
+						radius(distance(center(leg1), center(leg2))/2) {
+  }
+
+  Human(const Object& leg) : leg1(leg), point(center(leg)), radius(0.30) {
+  }
+
+  Human(const Point& point, const double radius): leg1(Object(point)), point(point), radius(radius) {}
+
+  Human(const Human& human): leg1(human.leg1), leg2(human.leg2), point(human.point), radius(human.radius) {} 
+
+  void operator=(const Human& human) {
+    leg1 = (human.leg1), leg2 = (human.leg2), point = (human.point), radius = (human.radius); }
+};
+
+static std::vector<Object> objects;
+static std::vector<Object> legs;
+static std::vector<Human> humans;
+
+static bool initialized = false;
+static Human trackingHuman(Object(Point(-10000, -10000)));
+
+#define CV_SHOW
+
+RTC::ReturnCode_t HumanFollower::onExecute(RTC::UniqueId ec_id)
+{
+  if (m_rangeIn.isNew()) {
+    m_rangeIn.read();
+    std::cout << "Range Data Received. (length=" << m_range.ranges.length() << ")" << std::endl;    
+    double maxDetection = 2.0;
+
+#ifdef CV_SHOW
+    uint32_t bufferImageCount = imageCount == 1 ? 0 : 1;
+    IplImage* image = pImages[bufferImageCount];
+    cvZero(image);
+#endif
+
+    objects.clear();
+    for (int i = 0; i < m_range.ranges.length(); i+=1) {
+      Point p = range_to_point(m_range.ranges[i], m_range.config.minAngle + i * m_range.config.angularRes);
+      
+      bool findObject = m_range.ranges[i] < maxDetection;
+      bool newObject = true;
+      if (i != 0) {
+	for(int j = 1;j <= 1;j++) {
+	  if (i - j > 0) {
+	    if (fabs(m_range.ranges[i-j] - m_range.ranges[i]) < 0.05*j) {
+	      newObject = false;
+	    }
+	  }
+	}
+      }
+
+      
+      if (findObject) {
+
+	if (newObject || objects.size() == 0) {
+	  objects.push_back(Object(p));
+	} else {
+	  objects[objects.size()-1].points.push_back(p);
+	}
+      }
+      
+      //auto color = m_range.ranges[i] > maxDetection ? CV_RGB(70, 70, 70) : colors[objects.size() % MAX_OBJECTS];
+#ifdef CV_SHOW
+      if (findObject) {
+	cvLine(image, cvPoint(WIDTH/2, HEIGHT / 2), toCvPoint(p), colors[objects.size() % MAX_OBJECTS], 1, 8, 0);
+      } else {
+	cvLine(image, cvPoint(WIDTH / 2, HEIGHT / 2), toCvPoint(p), CV_RGB(70, 70, 70), 1, 8, 0);
+      }
+#endif
+    }
+  
+    std::cout << "Objects = " << objects.size() << std::endl;
+    legs.clear();
+    humans.clear();
+    for (auto o: objects) {
+      double min_width_leg = 0.05;
+      double max_width_leg = 0.20;
+      double min_width_body = max_width_leg;
+      double max_width_body = 0.5;
+      
+      if (min_width_leg < width(o) && width(o) < max_width_leg) {
+	legs.push_back(o);
+      }
+
+      if (min_width_body < width(o) && width(o) < max_width_body) {
+	auto h = Human(center(o), width(o)/2);
+	humans.push_back(h);
+      }	
+    }
+
+    //std::cout << "Legs = " << legs.size() << std::endl;
+    for (auto leg : legs) {
+      auto c = center(leg);
+    }
+
+    if (legs.size() == 1) {
+      humans.push_back(Human(legs[0]));
+    }
+
+    for (int i = 1;i < legs.size();i++) {
+      double max_distance_legs = 1.0;
+      if (distance(center(legs[i-1]), center(legs[i])) < max_distance_legs) {
+	auto h = Human(legs[i-1], legs[i]);
+
+	humans.push_back(h);
+      } else {
+	humans.push_back(Human(legs[i-1]));
+      }
+    }
+
+    Human nextTrackingHuman = trackingHuman;
+    if (humans.size() > 0 && !initialized) {
+      for(auto h : humans) {
+	if (fabs(h.point.y) < fabs(trackingHuman.point.y)) {
+	  trackingHuman = h;
+	  nextTrackingHuman = h;
+	}
+      }
+      initialized = true;
+    } else if (humans.size() > 0) {
+      double max_distance = 10000;
+      for(auto h : humans) {
+	double d = distance(h.point, trackingHuman.point);
+	if (d < max_distance) {
+	  max_distance = d;
+	  nextTrackingHuman = h;
+	}
+      }
+    }
+
+    
+    double maxDistanceHumanWalk = 0.5;
+    //    if (distance(trackingHuman.point, nextTrackingHuman.point) < maxDistanceHumanWalk) {
+      trackingHuman = nextTrackingHuman;
+      //    }
+
+    std::cout << "Humans = " << humans.size() << std::endl;
+    
+#ifdef CV_SHOW
+    for(auto leg: legs) {
+      cvCircle(image, toCvPoint(center(leg)), width(leg) /2 / meter_by_pixel, colors[legs.size() % MAX_OBJECTS], -1);
+    }
+    for(auto h : humans) {
+      cvCircle(image, toCvPoint(h.point), h.radius / meter_by_pixel, colors[humans.size() % MAX_OBJECTS]);
+    }
+
+    if (initialized && objects.size() > 0) {
+      CvPoint hp = toCvPoint(trackingHuman.point);
+      cvLine(image, cvPoint(hp.x, 0), cvPoint(hp.x, HEIGHT), CV_RGB(0, 0, 255), 1, 4, 0);
+      cvLine(image, cvPoint(0, hp.y), cvPoint(WIDTH, hp.y), CV_RGB(0, 0, 255), 1, 4, 0);
+    }
+
+    cvLine(image, cvPoint(WIDTH / 2, 0), cvPoint(WIDTH / 2, HEIGHT), CV_RGB(255, 0, 0), 1, 4, 0);
+    cvLine(image, cvPoint(0, HEIGHT / 2), cvPoint(WIDTH, HEIGHT / 2), CV_RGB(255, 0, 0), 1, 4, 0);
+    imageCount = bufferImageCount;
+#endif
+    // human Tracked.
+    if (humans.size() > 0) {
+      std::cout << "[TrackingHuman] " << trackingHuman.point.x << ", " << trackingHuman.point.y << std::endl;
+      double dx = trackingHuman.point.x;
+      double dy = trackingHuman.point.y;
+
+      double offset = 1.0;
+      double dx_gain = 0.5;
+      double da_gain = 0.8;
+
+      double distance = sqrt(dx * dx + dy * dy) - offset;
+      double angle = atan2(dy, dx);
+      std::cout << "dth = " << angle << std::endl;
+      double vx = distance > 0 ? distance * distance * dx_gain : 0;
+      double vy = 0;
+      double va = angle * da_gain;
+
+      m_velocity.data.vx = vx;
+      m_velocity.data.vy = vy;
+      m_velocity.data.va = va;
+      setTimestamp(m_velocity);
+      m_velocityOut.write();      
+    }
+    
+  }
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t HumanFollower::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t HumanFollower::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t HumanFollower::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t HumanFollower::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t HumanFollower::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void HumanFollowerInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(humanfollower_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<HumanFollower>,
+                             RTC::Delete<HumanFollower>);
+  }
+  
+};
+
+
---
+++ src/particle_filter.cpp
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+
+#include <vector>
+
+double rand_double() {
+  return (double)rand() / (RAND_MAX);
+}
+
+double rand_normal(const double mu, const double sigma) {
+  return mu + sigma * (sqrt( -2.0*log(rand_double()) ) * sin(2.0*M_PI*rand_double()));
+}
+
+
+class position {
+public:
+  position() {}
+
+  position(double x, double y): x(x), y(y) {}
+
+
+  position(const position& p): x(p.x), y(p.y) {}
+  void operator=(const position& p) {
+    x = p.x; y = p.y;
+  }
+
+public:
+  double x, y;
+};
+
+
+class particle {
+public:
+  particle() {}
+
+  particle(double x, double y, double weight=0): x(x), y(y), w(weight) {}
+
+  particle(const particle& p): x(p.x), y(p.y), w(p.w) {}
+  void operator=(const particle& p) {
+    x = p.x; y = p.y; w = p.w;
+  }
+
+  ~particle() {}
+
+public:
+  double x;
+  double y;
+  double w;
+};
+
+class particle_list {
+public:
+  particle_list() {}
+
+  particle_list(uint32_t num_particles, double sx, double sy) {
+    for(uint32_t i = 0;i < num_particles;i++) {
+      particles.push_back(particle(rand_normal(0.0, sx), rand_normal(0.0, sy)));
+    }
+    max_weight = -1;
+    sum_weight = 0;
+  }
+
+  ~particle_list() {}
+
+  void copyFrom(const particle_list& pl) {
+    sum_weight = 0;
+    particles.clear();
+    for(auto p : pl.particles) {
+      particles.push_back(p);
+    }
+    max_weight = pl.max_weight;
+    sum_weight = pl.sum_weight;
+  }
+  
+  particle_list(const particle_list& pl) {
+    copyFrom(pl);
+  }
+
+  void operator=(const particle_list& pl) {
+    copyFrom(pl);
+  }
+
+public:
+  std::vector<particle> particles;
+  double max_weight;
+  double sum_weight;
+};
+
+class velocity2d {
+public:
+  velocity2d(double vx, double vy, double va) : vx(vx), vy(vy), va(va) {}
+public:
+  double vx;
+  double vy;
+  double va;
+};
+
+class motion {
+public:
+  velocity2d v;
+  double dt;
+  double sx;
+  double sy;
+public:
+  motion(const velocity2d v, double dt, double sx, double sy):
+    v(v), dt(dt), sx(sx), sy(sy) {}
+};
+
+class measurement {
+public:
+  measurement() {}
+  measurement(const measurement& m): p(m.p), sx(m.sx), sy(m.sy) {}
+  void operator=(const measurement& m) {
+    p = m.p; sx = m.sx; sy = m.sy;
+  }
+
+  ~measurement() {}
+
+public:
+  position p;
+  double sx;
+  double sy;
+};
+
+const particle apply_motion(const particle& pp, const motion& m) {
+  particle p;
+  p.x = pp.x + m.v.vx * m.dt + rand_normal(0.0, m.sx);
+  p.y = pp.y + m.v.vy * m.dt + rand_normal(0.0, m.sy);
+  return p;
+}
+
+
+const particle weight(const particle& p, const measurement& m) {
+  particle pp(p);
+  double dx = (m.p.x - p.x) / m.sx;
+  double dy = (m.p.y - p.y) / m.sy;
+
+  pp.w = sqrt(dx * dx + dy * dy);
+  return pp;
+}
+
+template<class T, const particle (*Func)(const particle&, const T &)>
+const particle_list map_particles(const particle_list& pl, const T& arg) { //, const particle (*Func)(const particle&, const T&)) {
+  particle_list plist;
+  double max_weight = -1;
+  plist.sum_weight = 0;
+  for (auto p : pl.particles) {
+    auto pp = Func(p, arg);
+    max_weight = pp.w > max_weight ? pp.w : max_weight;
+    plist.sum_weight += pp.w;
+    plist.particles.push_back(pp);
+  }
+  plist.max_weight = max_weight;
+  return plist;
+}
+
+template<class T, const T (*Func)(const particle_list& pl, const particle&, const T &)>
+const T reduce_particles(const particle_list& pl) {
+  T t;
+  for(auto p : pl.particles) {
+    t = Func(pl, p, t);
+  }
+  return t;
+}
+
+const particle_list apply_motion(const particle_list& pl, const motion& m) {
+  return map_particles<motion, &apply_motion>(pl, m);
+}
+
+const particle_list resampling(const particle_list& pl, const measurement& m) {
+  return map_particles<measurement, &weight>(pl, m);
+}
+
+
+const position sum_position(const particle_list& pl, const particle& p, const position& pos) {
+  position pp;
+  pp.x = pos.x + p.x / pl.sum_weight;
+  pp.y = pos.y + p.y / pl.sum_weight;
+  return pp;
+}
+
+const position mean_position(const particle_list& pl) {
+  return reduce_particles<position, &sum_position>(pl);
+}
+
+
